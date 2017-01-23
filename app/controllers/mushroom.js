293f91241085bf46168512d86111543d20f1e2b2
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-23 17:42:06 -0600

    created filter with ng-model

--- app/controllers/mushroom.js
+++ app/controllers/mushroom.js
@@ -3,19 +3,8 @@ app.controller('MushCtrl', function($scope, mushroomFactory){
     mushroomFactory.getMushrooms().then((val)=>{
         let objs = val.mushrooms
 
-        $scope.shrooms = function iterate(obj, stack) {
-                for (var property in obj) {
-                    if (obj.hasOwnProperty(property)) {
-                        if (typeof obj[property] == "object") {
-                            iterate(obj[property], stack + '.' + property);
-                        } else {
-                            console.log(property + "   " + obj[property]);
-                            $('#output').append($("<div/>").text(stack + '.' + property))
-                        }
-                    }
-                }
-            }
-
-        $scope.shrooms(objs, '')
+        // $scope.info = info
+        console.log(objs);
+        $scope.info = objs
     })
 })

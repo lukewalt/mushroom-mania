c19fa7c991a3cdbd14b251ba8dd38be8846fce86
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-24 17:35:21 -0600

    added logic for radio filters

--- app/controllers/mushroom.js
+++ app/controllers/mushroom.js
@@ -1,10 +1,9 @@
 app.controller('MushCtrl', function($scope, mushroomFactory){
     console.log("Using MushCtrl");
     mushroomFactory.getMushrooms().then((val)=>{
-        let objs = val.mushrooms
 
-        // $scope.info = info
-        console.log(objs);
-        $scope.info = objs
+        console.log($scope.info);
+        $scope.info = val.mushrooms
+
     })
 })
